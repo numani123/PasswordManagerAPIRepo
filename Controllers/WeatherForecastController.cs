ea6aea91c016761bd6b772c11020217ebc57446f
Numan <numan.i@hotmail.co.uk>
2025-12-31 23:24:18 +0000

    Implemented DI, class libraries, dtos, interfaces and services

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,33 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using PasswordManagerAPI.Application.Dtos;
+using PasswordManagerAPI.Application.Services;
 
 namespace PasswordManagerAPI.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
-    {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+   [ApiController]
+   [Route("[controller]")]
+   public class WeatherForecastController : ControllerBase
+   {
+      private readonly ILogger<WeatherForecastController> logger;
+      private readonly IWeatherForecastService weatherForecastService;
 
-        private readonly ILogger<WeatherForecastController> _logger;
+      public WeatherForecastController(ILogger<WeatherForecastController> logger,
+         IWeatherForecastService weatherForecastService)
+      {
+         this.weatherForecastService = weatherForecastService;
+         this.logger = logger;
+      }
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-    }
+      [HttpGet(Name = "GetWeatherForecast")]
+      public IEnumerable<WeatherForecastDto> Get()
+      {
+         return weatherForecastService.GetAllWeatherForecasts();
+      }
+   }
 }
